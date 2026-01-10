cb786638a2f1a515a57d3be737b8a0c7e4972e59
Ziyaa Danil M <ziyaadanil37@gmail.com>
2026-01-10 11:09:58 +0700

    perbaikan tugas p6 untuk try catch

--- Tugas/pertemuan-6/src/routes/eventRouter.ts
+++ Tugas/pertemuan-6/src/routes/eventRouter.ts
@@ -5,61 +5,87 @@ import { eventValidation } from "../validation/event-validation.js";
 
 export const eventsRoute = new Hono()
   .get("/", async (c) => {
-    const events = await prisma.event.findMany({
-      include: {
-        participants: true,
-      },
-    });
-    return c.json({ events });
+    try {
+      const events = await prisma.event.findMany({
+        include: {
+          participants: true,
+        },
+      });
+      return c.json({ data : events }, 200);
+    } catch (error) {
+      console.log("Failed listing events:", error);
+      // status code 200: default OK, 201: created, 400: bad request,404: not found, 500 : server error
+      return c.json({message : "Failed listing events"}, 500)
+    }
   })
   .get("/:id", async (c) => {
     const id = c.req.param("id");
-    const event = await prisma.event.findFirst({
-      where: {
-        id: id,
-      },
-      include: {
-        participants: true,
-      },
-    });
-    return c.json({ event });
+    try {
+      const event = await prisma.event.findFirst({
+        where: {
+          id: id,
+        },
+        include: {
+          participants: true,
+        },
+      });
+      return c.json({ data : event }, 200);
+    } catch (error) {
+      console.log(`Failed getting event ${id}: `, error);
+      return c.json({message : `Failed getting event ${id}`}, 500)
+    }
   })
   .post("/", zValidator("json", eventValidation), async (c) => {
-    const body = c.req.valid("json");
-    const newEvent = await prisma.event.create({
-      data: {
-        name: body.name,
-        description: body.description,
-        location: body.location,
-        dateTime: body.dateTime,
-      },
-    });
-    return c.json({ event: newEvent });
+    try {
+      const body = c.req.valid("json");
+      const newEvent = await prisma.event.create({
+        data: {
+          name: body.name,
+          description: body.description,
+          location: body.location,
+          dateTime: body.dateTime,
+        },
+      });
+      return c.json({ data: newEvent }, 201);
+    } catch (error) {
+      console.log("Failed creating event:", error);
+      return c.json({message : "Failed creating event"}, 500)
+    }
   })
   .patch("/:id", zValidator("json", eventValidation), async (c) => {
     const id = await c.req.param("id");
-    const body = c.req.valid("json");
-    const updateEvent = await prisma.event.update({
-      where: {
-        id: id,
-      },
-      data: {
-        name: body.name,
-        description: body.description,
-        location: body.location,
-        dateTime: body.dateTime,
-      },
-    });
-    return c.json({ event: updateEvent });
+    try {
+      const body = c.req.valid("json");
+      const updateEvent = await prisma.event.update({
+        where: {
+          id: id,
+        },
+        data: {
+          name: body.name,
+          description: body.description,
+          location: body.location,
+          dateTime: body.dateTime,
+        },
+      });
+      return c.json({ data: updateEvent }, 200);
+    } catch (error) {
+      console.log(`Failed updating event ${id}: `, error);
+      return c.json({message : `Failed updating event ${id}`}, 500)
+    }
   })
   .delete("/:id", async (c) => {
     const id = c.req.param("id");
-    await prisma.event.delete({
-      where: {
-        id: id,
-      },
-    });
-    return c.json({ message: "Successfull delete event" });
+    try {
+      await prisma.event.delete({
+        where: {
+          id: id,
+        },
+      });
+      return c.json({ message: "Successfull delete event" }, 200);
+    } catch (error) {
+      console.log(`Failed deleting event ${id}: `, error);
+      return c.json({message : `Failed deleting event ${id}`}, 500)
+    }
   });
 
 
--- Tugas/pertemuan-6/src/routes/eventparticipant.ts
+++ Tugas/pertemuan-6/src/routes/eventparticipant.ts
@@ -5,22 +5,36 @@ import { participantValidation } from "../validation/participant-validation.js";
 
 export const participantsRoute = new Hono()
     .get('/', async (c) => {
-        const participant = await prisma.participant.findMany();
-        return c.json({ participant })
+        try {
+          const participant = await prisma.participant.findMany();
+          return c.json({ data: participant }, 200)
+        } catch (error) {
+          console.log("Failed listing participants:", error);
+          return c.json({message : "Failed listing participants"}, 500)
+        }
     })
     .get('/:id', async (c) => {
         const id = c.req.param('id')
-        const participant = await prisma.participant.findFirst({
-          where: {
-            id: id,
-          },
-        });
-        return c.json({
-            participant
-        })
+        try {
+          const participant = await prisma.participant.findFirst({
+            where: {
+              id: id,
+            },
+          });
+          return c.json({ data: participant }, 200)
+        } catch (error) {
+          console.log(`Failed getting participant ${id}: `, error);
+          return c.json({message : `Failed getting participant ${id}`}, 500)
+        }
     })
     .post('/', zValidator("json", participantValidation), async (c) => {
+      try {
         const body =  c.req.valid("json");
+        const idEvent = await prisma.event.findUnique({  where : {id : body.eventId } });
+        if (!idEvent) {
+          console.log("Failed finding id event:");
+          return c.json({message : "Failed finding id event"}, 500)
+        }
         const newParticipant = await prisma.participant.create({
           data: {
             name: body.name,
@@ -28,31 +42,45 @@ export const participantsRoute = new Hono()
             eventId : body.eventId
           },
         });
-        return c.json({ event: newParticipant });
+        return c.json({ data: newParticipant }, 201);
+      } catch (error) {
+        console.log("Failed creating participant:", error);
+        return c.json({message : "Failed creating participant"}, 500)
+      }
     })
     .patch('/:id', zValidator("json", participantValidation), async (c) => {
         const id = await c.req.param("id");
-        const body = c.req.valid("json");
-        const updateParticipant = await prisma.participant.update({
-          where: {
-            id: id,
-          },
-          data: {
-            name: body.name,
-            email: body.email,
-            eventId: body.eventId,
-          },
-        });
-        return c.json({ event: updateParticipant });
+        try {
+          const body = c.req.valid("json");
+          const updateParticipant = await prisma.participant.update({
+            where: {
+              id: id,
+            },
+            data: {
+              name: body.name,
+              email: body.email,
+              eventId: body.eventId,
+            },
+          });
+          return c.json({ data: updateParticipant }, 200);
+        } catch (error) {
+          console.log(`Failed updating participant ${id}: `, error);
+          return c.json({message : `Failed updating participant ${id}`}, 500)
+        }
     })
     .delete('/:id', async (c) => {
         const id = c.req.param("id");
-        await prisma.participant.delete({
-          where: {
-            id: id,
-          },
-        });
-        return c.json({ message: "Successfull delete Participant" });
+        try {
+          await prisma.participant.delete({
+            where: {
+              id: id,
+            },
+          });
+          return c.json({ message: "Successfull delete Participant" }, 200);
+        } catch (error) {
+          console.log(`Failed deleting participant ${id}: `, error);
+          return c.json({message : `Failed deleting participant ${id}`}, 500)
+        }
     })
 
 
