0631d717ef124e06791d0666564398a976d6dae8
Ziyaa Danil M <ziyaadanil37@gmail.com>
2026-02-16 07:59:49 +0700

    update remote

--- pertemuan-15/apps/api/src/index.ts
+++ pertemuan-15/apps/api/src/index.ts
@@ -1,15 +1,30 @@
 import { serve } from "@hono/node-server";
 import { Hono } from "hono";
 import { prisma } from "./utils/prisma.js";
+import { zValidator } from "@hono/zod-validator";
+import { createUserSchema } from "./modules/users/schema.js";
 
-const app = new Hono();
-
-app.get("/", async (c) => {
+const app = new Hono().get("/users", async (c) => {
 	const users = await prisma.user.findMany();
 	return c.json({
-		users,
+		users
 	});
-});
+}).post("/users", zValidator("json", createUserSchema), async (c) => {
+	const body = c.req.valid("json");
+	const user = await prisma.user.create({
+		data : {
+			name: body.name,
+			email: body.email,
+		}
+	});
+
+	return c.json({
+		user
+	});
+})
+
+// export api scpecification
+export type AppType = typeof app;
 
 serve(
 	{
--- pertemuan-15/apps/platform/src/routes/index.tsx
+++ pertemuan-15/apps/platform/src/routes/index.tsx
@@ -1,7 +1,21 @@
+import { api } from "@/utils/api";
 import { createFileRoute } from "@tanstack/react-router";
 
-export const Route = createFileRoute("/")({ component: App });
+export const Route = createFileRoute("/")({ component: App, loader: async() => {
+	const res = await api.users.$get();
+	const data = await res.json();
+	return data;
+} });
 
 function App() {
-	return <div>Hello platform</div>;
+	const data = Route.useLoaderData();
+	return (
+		<div>
+			<div>{data.users.map((user) => {
+				return (
+					<div key={user.id}>{user.name}</div>
+				)
+			})}</div>
+		</div>
+	)
 }
